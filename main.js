5c257e72685b86c0f17746969fbc80f9e17d6886
Elyager <elyager@gmail.com>
2018-09-11 16:19:46 -0700

    Add some makeup 💄

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-  var $canvas = document.getElementById('canvas');
-  var context = $canvas.getContext('2d');
-  var img = new Image();
+  let $canvas = document.getElementById('canvas');
+  let $context = $canvas.getContext('2d');
+  let img = new Image();
   img.src = 'cara.jpg';
 
-  var w, h, offset, glitchInterval;
-  
-  img.onload = function() {
-    init();
-    window.onresize = init;
-  }
+  let w, h, offset, glitchInterval;
 
-  var init = function() {
+  img.addEventListener('load', () => {
+    init();
+    window.addEventListener('resize', init);
+  });
+
+  function init() {
     clearInterval(glitchInterval); //remover el intervalo anterior
     $canvas.width = w = img.width + 30;
     offset = w * .1; //offset de un 10% del ancho
     $canvas.height = h = img.height;
-    
-    glitchInterval = setInterval(function() {
+
+    glitchInterval = setInterval(() => {
       clear();
-      context.drawImage(img, -60, 0, img.width, img.height, -30, 0, w, h);
+      $context.drawImage(img, -60, 0, img.width, img.height, -30, 0, w, h);
       setTimeout(glitchImg, randInt(250, 1000));
-    }, 500);
-  };
+    }, 300);
+  }
 
-  var glitchImg = function() {
-    for (var i = 0; i < randInt(1, 13); i++) {
-      var x = Math.random() * w;
-      var y = Math.random() * h;
-      var spliceWidth = w - x;
-      var spliceHeight = randInt(5, h / 3);
-      context.drawImage($canvas, 0, y, spliceWidth, spliceHeight, x, y, spliceWidth, spliceHeight);
-      context.drawImage($canvas, spliceWidth, y, x, spliceHeight, 0, y, x, spliceHeight);
+  function glitchImg() {
+    for (let i = 0; i < randInt(1, 13); i++) {
+      let x = Math.random() * w;
+      let y = Math.random() * h;
+      let spliceWidth = w - x;
+      let spliceHeight = randInt(5, h / 3);
+      $context.drawImage($canvas, 0, y, spliceWidth, spliceHeight, x, y, spliceWidth, spliceHeight);
+      $context.drawImage($canvas, spliceWidth, y, x, spliceHeight, 0, y, x, spliceHeight);
     }
-  };
+  }
 
-  var clear = function() {
-    context.rect(0, 0, w, h);
-    context.fill();
-  };
+  function clear() {
+    $context.rect(0, 0, w, h);
+    $context.fill();
+  }
 
-  var randInt = function(min, max) {
+  function randInt(min, max) {
     return Math.floor((Math.random() * (max - min) + min));
-  };
+  }
